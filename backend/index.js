3f09c49a9812e18e96892c44e11c46a86d37026b
manoharbondalapati <manohar.bondalapati34@gmail.com>
2024-08-03 17:58:20 +0530

    updated files

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 mongoose
-  .connect("mongodb://localhost:27017/todos", {
+  .connect("mongodb+srv://manoharbondalapati34:Y1JG1GM1MJMM9ubA@todocluster.0w1k4km.mongodb.net/?retryWrites=true&w=majority&appName=todocluster", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
